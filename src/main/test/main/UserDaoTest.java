19b009d390aa44f5e0253581cf197358380e16a7
huuuuuuuuux <huxley.xiaohan@gmail.com>
2020-03-27 17:17:47 +0800

    update

---
+++ src/main/test/main/UserDaoTest.java
@@ -0,0 +1,30 @@
+import hospitalServer.dao.UserDao;
+import hospitalServer.bean.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @author: create by XiaoHan
+ * @version: v1.0
+ * @description: hospitalServer.bean
+ * @date:2020/3/27
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring-mybatis.xml"})
+public class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testFindUserById() {
+        int id = 1;
+        List<User> user = userDao.showUser();
+        System.out.println(user.get(0).getId() + ":" + user.get(0).getName());
+    }
+}
